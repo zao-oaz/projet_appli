d335f0ee9049690c4d586e8f4b22979c5c24efc7
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-26 16:59:09 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -8,8 +8,3 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
-
-df.isnull().sum().plot(kind='hist', figsize=(12,7), color="orange")
-plt.title('Distribution of NaNs')
-plt.xlabel('NaNs')
-plt.show()
