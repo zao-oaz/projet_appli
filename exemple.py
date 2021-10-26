bcb0639aa874bec3d2ae3ddadfb3cf6956409378
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-26 19:49:37 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import streamlit as st
-from matplotlib import figure
-from matplotlib.backends.backend_agg import RendererAgg
-_lock = RendererAgg.lock
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Hello world!")
