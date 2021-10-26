e2fa2b5041bba296a8285b355e58e9ccd55d81a0
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-26 16:53:28 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,24 +1,12 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Hello world!")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
+  df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
 
-  # Add some matplotlib code !
-  fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="Age",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf91",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
-  st.write(fig)
+
