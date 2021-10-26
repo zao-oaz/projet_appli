b42154c24cbb7976325838523a84c2dc4a0befcd
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-26 16:39:43 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-import matplotlib
-@st.cache(hash_funcs={matplotlib.figure.Figure: lambda _: None})
-def mk_figure
-
 st.title("Hello world!")
 
 uploaded_file = st.file_uploader("Choose a file")
@@ -12,16 +8,3 @@ if uploaded_file is not None:
   df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
 
-# Add some matplotlib code !
-  fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="nutriscore_grade",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf91",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
-  st.write(fig)
