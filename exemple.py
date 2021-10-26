f72e8f04f83b9d09d0c969e57cab6f0e71314482
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-26 16:27:38 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,13 +1,9 @@
-
+# app.py, run with 'streamlit run app.py'
+import pandas as pd
 import streamlit as st
 
-st.title("nutrition_app")
+df = pd.read_csv("C:/Users/zaome/Documents/Alisson/Flask/database_clean.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+# df = pd.read_excel(...)  # will work for Excel files
 
-import pandas as pd
-
-df = pd.read_csv("C:\Users\zaome\Documents\Alisson\Flask\database_clean.csv", sep="|")
-
-import plotly.express as px
-
-corr = df.corr()
-px.imshow(corr)
+st.title("Hello world!")  # add a title
+st.write(df)  # visualize my dataframe in the Streamlit app
