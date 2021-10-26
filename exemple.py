77bc462c86511f9a86c0df263aa530d5bef2c0fc
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-26 19:45:55 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.title("Hello world!")
@@ -10,9 +8,3 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
