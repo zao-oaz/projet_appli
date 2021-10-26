47b83c9fb252c7f45668380a067694af10c162b3
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-26 16:37:07 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+import matplotlib.pyplot as plt
 
 st.title("Hello world!")
 
@@ -9,6 +9,16 @@ if uploaded_file is not None:
   df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
 
-  
-corr = df.corr()
-px.imshow(corr)
+# Add some matplotlib code !
+  fig, ax = plt.subplots()
+  df.hist(
+    bins=8,
+    column="nutriscore_grade",
+    grid=False,
+    figsize=(8, 8),
+    color="#86bf91",
+    zorder=2,
+    rwidth=0.9,
+    ax=ax,
+  )
+  st.write(fig)
