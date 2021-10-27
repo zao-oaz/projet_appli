091d3da78ef0e9b1bb8a45a0a0d8241453962ac7
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-27 11:23:30 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -15,7 +15,8 @@ if uploaded_file is not None:
 option = st.selectbox(
   'Sélectionner vos critères alimentaires'
   ('Sucre', 'Sel', 'Fibres', 'Proteines'))
-st.write()  
+
+st.write('You selected:', option)  
 
 txt = st.text_area('Le sel', ''' 4g de sel sont nécessaires au bon fonctionnement de l'organisme : transmission des 
 signaux nerveux, contraction musculaire et fonctionnement des reins en assurant une bonne hydratation. Le sel gouverne, avec le potassium, tout l'équilibre hydrique de l'organisme.''')
