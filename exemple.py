ba9b9cb57736b2e9494563bd126f9bd23f4ffb81
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-27 10:39:06 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -13,14 +13,14 @@ if uploaded_file is not None:
  
 
 fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="salt_100g",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf91",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
-  st.write(fig)
+df.hist(
+  bins=8,
+  column="salt_100g",
+  grid=False,
+  figsize=(8, 8),
+  color="#86bf91",
+  zorder=2,
+  rwidth=0.9,
+  ax=ax,
+)
+st.write(fig)
