ab2e6604ae4714c60a2d5fa2b5ee389cebbe063a
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-27 11:13:37 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -18,13 +18,13 @@ option = st.selectbox(
 
 st.write('You selected:', option)  
 
->>> txt = st.text_area('Text to analyze', '''
+txt = st.text_area('Text to analyze', '''
 ...     4g de sel sont nécessaires au bon fonctionnement de l'organisme : 
 ...     transmission des signaux nerveux, contraction musculaire et 
 ...     fonctionnement des reins en assurant une bonne hydratation. 
 ...     Le sel gouverne, avec le potassium, tout l'équilibre hydrique de l'organisme. (...)
 ...     ''')
->>> st.write('Sentiment:', run_sentiment_analysis(txt))
+st.write('Sentiment:', run_sentiment_analysis(txt))
 
 #Visualisations1
 st.header('Répartition du sel')
