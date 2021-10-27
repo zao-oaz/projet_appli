f80aa725e7a5fe170f0e9f366b40caa22cca0963
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-27 11:21:05 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -20,7 +20,7 @@ st.write('You selected:', option)
 
 txt = st.text_area('Le sel', ''' 4g de sel sont nécessaires au bon fonctionnement de l'organisme : transmission des 
 signaux nerveux, contraction musculaire et fonctionnement des reins en assurant une bonne hydratation. Le sel gouverne, avec le potassium, tout l'équilibre hydrique de l'organisme.''')
-st.write((txt))
+st.write()
 
 #Visualisations1
 st.header('Répartition du sel')
