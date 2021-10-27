175683d4dae76ea82e9af9d622a5a1bed93c3e60
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-27 09:58:57 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 import matplotlib.pyplot as plt
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -9,4 +10,6 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
-  
+ 
+corr = df.corr()
+px.imshow(corr)
