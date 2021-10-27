3aa241b9b92948bbac38583402d9c42b72baa9f4
zao-oaz <79938367+zao-oaz@users.noreply.github.com>
2021-10-27 10:53:06 +0200

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -8,7 +8,7 @@ st.title("Bienvenue sur notre appli Nutri-Sport !")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
+  df = pd.read_csv(uploaded_file, sep="|")
   st.write(df)
   
 #Visualisations
